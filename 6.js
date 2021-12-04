a980f5644dc1a1f9df5023c93096a8f7da43bad9
Екатерина Новоторцева <93860170+KatyNova@users.noreply.github.com>
2021-12-05 01:42:25 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,6 +1,6 @@
-var str = "123456";
-var sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
-var sum2 = Number(str[3]) + Number(str[4]) + Number(str[5])
+let str = "123456";
+let sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
+let sum2 = Number(str[3]) + Number(str[4]) + Number(str[5])
 if (sum1 == sum2) {
 alert("Да");
 } else {
